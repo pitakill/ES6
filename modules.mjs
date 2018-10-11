a3acb2c2e9acf27eab3870571eadae4874cfdc0a
Lepoldo Caballero <git@pitakill.net>
2018-10-11 17:16:38 -0500

    Add Modules

---
+++ modules.mjs
@@ -0,0 +1,12 @@
+import {
+  suma,
+  resta,
+  multiplicacion,
+  division
+} from './modules-operations'
+
+console.log(suma(1, 2))
+console.log(resta(1, 2))
+console.log(multiplicacion(1, 2))
+console.log(division(1, 2))
+
